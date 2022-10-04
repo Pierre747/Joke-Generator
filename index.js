6e8c8db42f64953c75a2a3d7bfae131767101621
Pierre RICARD <93609658+Pierre747@users.noreply.github.com>
2022-10-04 18:25:32 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const header = document.getElementById('header');
+const content = document.getElementById('content');
+
+console.log(header, content);
+
+const getJoke = () => {
+	fetch('https://api.blablagues.net/?rub=blagues')
+		.then((res) => res.json())
+		.then((data) => {
+			const joke = data.data.content; // afin de destructurer la réponse et rendre le contenu lisible
+			header.textContent = joke.text_head;
+			content.textContent =
+				joke.text !== '' // Pour afficher les réponses aux devinettes qui sont cachées par l'API
+					? joke.text
+					: joke.text_hidden;
+		});
+};
+getJoke();
+
+document.body.addEventListener('click', () => {
+	getJoke();
+});
